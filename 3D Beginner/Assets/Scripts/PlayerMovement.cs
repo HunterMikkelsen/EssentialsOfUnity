f5bcd3c8fb2e9a9f95e60b987311185a612ec63e
Scuvy <59233814+Scuvy@users.noreply.github.com>
2021-09-14 16:00:21 -0600

    Finished tutorials & added a new project
    Finished John Lemon's Haunted Jaunt: Part 1 & exploring & learning unity tutorials.

---
+++ 3D Beginner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+
+    float horizontal;
+    float vertical;
+    bool hasHorizontalInput;
+    bool hasVerticalInput;
+    bool isWalking;
+    Vector3 desiredForward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+}
