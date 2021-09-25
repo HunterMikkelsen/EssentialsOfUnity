05c62ee9e54e7b0d6f3ee9accb83c21556625a60
Scuvy <59233814+Scuvy@users.noreply.github.com>
2021-09-25 15:56:53 -0600

    Completed John Lemon Haunted Jaunt
    Added Enemies (pathing, models, prefabs), Added Audio to the scene and enemies, Changed Primary Audio Listener, Added scripts

--- 3D Beginner/Assets/Scripts/PlayerMovement.cs
+++ 3D Beginner/Assets/Scripts/PlayerMovement.cs
@@ -6,41 +6,45 @@ public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
 
-    Vector3 m_Movement;
-    Quaternion m_Rotation = Quaternion.identity;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
 
-    float horizontal;
-    float vertical;
-    bool hasHorizontalInput;
-    bool hasVerticalInput;
-    bool isWalking;
-    Vector3 desiredForward;
-
-    // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
 
-        hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        hasVerticalInput = !Mathf.Approximately(vertical, 0f);
-        isWalking = hasHorizontalInput || hasVerticalInput;
-
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
 
-        desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
     }
 
@@ -49,5 +53,4 @@ public class PlayerMovement : MonoBehaviour
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
         m_Rigidbody.MoveRotation(m_Rotation);
     }
-
 }
